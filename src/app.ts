75979d93e42645dd144e41c9d7c36ae78486cceb
Markus Persson <persik7707@outlook.com>
2022-02-26 13:25:27 +0100

    fix

--- src/app.ts
+++ src/app.ts
@@ -32,18 +32,6 @@ app.use("/api/upload", uploadRouter);
 
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "client", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
-}
-
 app.use(notFound);
 app.use(errorHandler);
 
